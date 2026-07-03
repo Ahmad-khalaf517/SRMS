b55e1cdd1cea6753409bd667a89e50b838929ef8
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-07-04 02:38:26 +0300

    Create pnpm workspaces

---
+++ apps/backend/src/server.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+
+app.get('/api', (req, res) => res.json({ message: "Hello from Express!" }));
+app.listen(5000, () => console.log('Server running on port 5000'));
