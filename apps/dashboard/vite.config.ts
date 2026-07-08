053ebd6718bb421f4895260abbcdc3a689a810f6
Ahmad Khalaf <ahmadkhalaf517@gmail.com>
2026-07-08 09:55:34 +0300

    Add vite port to front-end project

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -1,14 +1,20 @@
-import path from "path"
-import { defineConfig } from 'vite';
-import tailwindcss from "@tailwindcss/vite"
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-})
+    server: {
+      port: Number(env.VITE_PORT),
+    },
+  };
+});
